8b8585048e4b4a15211f9f5d8dbb3c0fe73d4292
madirgendwas <am@schmidhuber.com>
2018-04-23 16:37:50 +0200

    nach heute

--- node/server-expressort.js
+++ node/server-expressort.js
@@ -3,10 +3,12 @@ var express = require ('express');
 var bp      = require ('body-parser');
 
 
+var ortarray = [];
+
 var app = express();
 
-var server = app.listen(5000, function(){
-    console.log("Express Server gestartet : localhost:5000");
+var server = app.listen(5001, function(){
+    console.log("Express Server gestartet : localhost:5001");
 })
 
 app.use(function(req, res, next) {
@@ -17,6 +19,7 @@ app.use(function(req, res, next) {
 })
 
 app.use( express.static('static'))
+app.use(bp.urlencoded({extended:true}));
 
 app.post( '/', function(req, res){
     res.setHeader('content-type', 'text/html')
@@ -24,9 +27,22 @@ app.post( '/', function(req, res){
     res.end('OK');
 })
 
+app.post( '/ort', function(req, res){
+    var meinort = JSON.stringify(req.body);
+    console.log(meinort);
+    ortarray[ortarray.length] = meinort;
+
+    fs.writeFile('meineort', ortarray, function(){});
+
+    res.setHeader('content-type', 'text/html')
+    res.writeHeader (200);
+    res.end('OK');
+})
+
+
 app.get( '/', function(req, res){
     res.writeHeader (200);
-    fs.readFile('d12-server.html', function(err, data){
+    fs.readFile('d12-serverort.html', function(err, data){
         res.end(data);
     });
 })
--- node/startme.js
+++ node/startme.js
@@ -2,12 +2,21 @@ var fs = require ("fs");
 var cp = require ("child_process");
 
 var file = "server-express.js";
+var file1 = "server-expressort.js";
+
 var server = cp.fork(file); // starte den server
+var server1 = cp.fork(file1); // starte den server
 console.log("first time start");
 fs.watchFile(file, function(){
     server.kill();
     console.log("stop service");
     server = cp.fork(file);
     console.log("start service");
+})
 
+fs.watchFile(file1, function(){
+    server1.kill();
+    console.log("stop service1");
+    server1 = cp.fork(file1);
+    console.log("start service1");
 })
